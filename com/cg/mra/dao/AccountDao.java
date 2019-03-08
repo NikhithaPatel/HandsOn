c122142ad79615de43fb90a8ac698d00040f2e79
NikhithaPatel <48005729+NikhithaPatel@users.noreply.github.com>
2019-03-08 17:25:19 +0530

    Update AccountDao.java

--- com/cg/mra/dao/AccountDao.java
+++ com/cg/mra/dao/AccountDao.java
@@ -1,1 +1,9 @@
+package com.cg.mra.dao;
+import com.cg.mra.service.*;
+import com.cg.mra.beans.Account;
+
+public interface AccountDao {
+Account getAccountDetails(String mobileNo);
+int rechargeAccount(String mobileNo,int rechargeAmount);
+}
 
