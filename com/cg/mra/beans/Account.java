424d68f6b07b0698ec580c0dfa0685395a865107
NikhithaPatel <48005729+NikhithaPatel@users.noreply.github.com>
2019-03-08 17:24:15 +0530

    Update Account.java

--- com/cg/mra/beans/Account.java
+++ com/cg/mra/beans/Account.java
@@ -1,1 +1,32 @@
+package com.cg.mra.beans;
+
+public class Account {
+private	String accountType;
+private	String customerName;
+private int AccountBalance;
+public Account(String string,String string1,int i) {
+	accountType=string;
+    customerName=string1;
+	AccountBalance=i;
+}
+public String getAccountType() {
+	return accountType;
+}
+public void setAccountType(String accountType) {
+	this.accountType = accountType;
+}
+public String getCustomerName() {
+	return customerName;
+}
+public void setCustomerName(String customerName) {
+	this.customerName = customerName;
+}
+public int getAccountBalance() {
+	return AccountBalance;
+}
+public void setAccountBalance(int accountBalance) {
+	AccountBalance = accountBalance;
+}
+
+}
 
